4ed8e6d1347d514195abf46df9874e569f10f83b
Sandra Sanders <sandy-sanders@msn.com>
2022-03-22 20:54:11 -0700

    homepage

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,70 +1,28 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function Homepage() {
 
     return (
-        <div uk-filter="target: .js-filter">
-
-            <ul class="uk-subnav uk-subnav-pill uk-flex uk-flex-center">
-                <li class="uk-active" uk-filter-control=''><a href="#">All Products</a></li>
-                <li uk-filter-control="[data-color='white']"><a href="#">Food</a></li>
-                <li uk-filter-control="[data-color='blue']"><a href="#">Toys</a></li>
-                <li uk-filter-control="[data-color='black']"><a href="#">Gear</a></li>
-                <li uk-filter-control="[data-color='Health']"><a href="#">Health</a></li>
-            </ul>
-
-            <ul class="js-filter uk-child-width-1-2 uk-child-width-1-3@m uk-text-center" uk-grid=''>
-                <li data-color="white">
-                    <div className="uk-child-width-1-2@m" uk-grid>
-                        <div>
-                            <div class="uk-card uk-card-default">
-                                <div class="uk-card-media-top">
-                                    <img src="./src/assets/images/food/bbtastefulcat.png" alt="" />
-                                </div>
-                                <div class="uk-card-body">
-                                    <h3 class="uk-card-title">Media Top</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li data-color="blue">
-                    <div class="uk-card uk-card-primary uk-card-body">Item</div>
-                </li>
-                <li data-color="white">
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </li>
-                <li data-color="white">
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </li>
-                <li data-color="black">
-                    <div class="uk-card uk-card-secondary uk-card-body">Item</div>
-                </li>
-                <li data-color="black">
-                    <div class="uk-card uk-card-secondary uk-card-body">Item</div>
-                </li>
-                <li data-color="blue">
-                    <div class="uk-card uk-card-primary uk-card-body">Item</div>
-                </li>
-                <li data-color="black">
-                    <div class="uk-card uk-card-secondary uk-card-body">Item</div>
-                </li>
-                <li data-color="blue">
-                    <div class="uk-card uk-card-primary uk-card-body">Item</div>
-                </li>
-                <li data-color="white">
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </li>
-                <li data-color="blue">
-                    <div class="uk-card uk-card-primary uk-card-body">Item</div>
-                </li>
-                <li data-color="black">
-                    <div class="uk-card uk-card-secondary uk-card-body">Item</div>
-                </li>
-            </ul>
-        </div>
+        <Row xs={1} md={2} className="g-4">
+            {Array.from({ length: 4 }).map((_, idx) => (
+                <Col>
+                    <Card>holder.js/1
+                        <Card.Img variant="top" src="" />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            ))}
+        </Row>
     )
-}
+};
 
 export default Homepage;
